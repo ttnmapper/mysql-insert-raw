932c5496528e5dc3483467cb343da346567d1c20
JP Meijers <gitkraken@jpmeijers.com>
2019-07-01 00:09:24 +0200

    Prometheus exporter should listen on all interfaces so that other containers can see the metrics

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ func main() {
 
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
-		err := http.ListenAndServe("127.0.0.1:"+myConfiguration.PrometheusPort, nil)
+		err := http.ListenAndServe(":"+myConfiguration.PrometheusPort, nil)
 		if err != nil {
 			log.Print(err.Error())
 		}
